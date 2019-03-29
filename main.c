4d7f64eb5cc6bfd931865b4bc089fc5067ba04d3
Luigi Ferdinando <luigiiazzetta98@gmail.com>
2019-03-29 11:41:49 +0100

    Added main for phase 1.5

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <libuarm.h>
+#include <uARMconst.h>
+#include <uARMtypes.h>
+#include <arch.h>
+
+#include "tests/p1.5/p1.5test_rikaya_v0.c"
+#include "pcb/pcb.h"
+
+#include "scheduler.h"
+#include "exceptions.h"
+#include "interrupts.h"
+
+
+
+void initArea(memaddr area, memaddr handler){
+    state_t *newArea = (state_t*) area;
+    /* Memorizza il contenuto attuale del processore in newArea */
+    STST(newArea);
+    /* Setta pc alla funzione che gestirà l'eccezione */
+    newArea->pc = handler;
+    /* Setta sp a RAMTOP */
+    newArea->sp = RAM_TOP;
+    /* Setta il registro di Stato per mascherare tutti gli interrupt e si mette in kernel-mode. */
+    newArea->cpsr = STATUS_ALL_INT_DISABLE((newArea->cpsr) | STATUS_SYS_MODE);
+    /* Disabilita la memoria virtuale */
+    newArea->CP15_Control = (newArea->CP15_Control) & ~(ENABLE_VM);
+}
+
+
+int main() {
+
+  /* Popola le 4 nuove Aree nella ROM */
+  initArea(INT_NEWAREA,       (memaddr) intHandler);
+  initArea(TLB_NEWAREA,       (memaddr) tlbHandler);
+  initArea(PGMTRAP_NEWAREA,   (memaddr) pgmHandler);
+  initArea(SYSBK_NEWAREA,     (memaddr) sysBpHandler);
+
+
+  /* Inizializza la stuttura dati dei pcbs */
+  initPcbs();
+
+  /* Inizializza la lista dei processi (readyQueue) */
+  LIST_HEAD(readyQueue);
+
+  /* Alloco i 3 processi e li aggiungo alla readyQueue */
+  for (int i = 1; i <= 3; i++) {
+    pcb_t *Process = allocPcb();
+    if (Process == NULL) {
+      PANIC();
+    }
+
+    /* Abilita gli interrupt, il Local Timer e la kernel-mode */
+    Process->p_s.cpsr = STATUS_ALL_INT_ENABLE(Process->p_s.cpsr) | STATUS_SYS_MODE;
+    /* Disabilita la memoria virtuale */
+    Process->p_s.CP15_Control = (Process->p_s.CP15_Control) & ~(ENABLE_VM);
+    /* Assegna ad SP il valore (RAMTOP - FRAMESIZE) * la priorità i */
+    Process->p_s.sp = (RAM_TOP - FRAME_SIZE) * i;
+    /* Assegno la priorità e l'original priority uguale a numero del test */
+    Process->original_priority = Process->priority = i;
+
+    /* Assegno i vari test (1, 2, 3) a seconda del valore di i */
+    switch (i) {
+      case 1:
+        Process->p_s.pc = (memaddr) test1;
+      break;
+
+      case 2:
+        Process->p_s.pc = (memaddr) test2;
+      break;
+
+      case 3:
+        Process->p_s.pc = (memaddr) test3;
+      break;
+    }
+
+
+    insertProcQ(&readyQueue, Process);
+  }
+
+  schedule();
+}
