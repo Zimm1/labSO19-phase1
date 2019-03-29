1226e5bfff7ae114e59eca014a0e07bfbee1f955
Stefano <stefano.notari1998@gmail.com>
2019-03-29 19:13:36 +0100

    Added right libraries and fixed log_process_order call.

--- scheduler.c
+++ scheduler.c
@@ -1,24 +1,18 @@
-#include <libuarm.h>
-#include <uARMconst.h>
-#include <uARMtypes.h>
-#include <arch.h>
+#include <umps/arch.h>
+#include <umps/libumps.h>
 
 #include "pcb/pcb.h"
-#include "interrupt.h"
-
 
 unsigned int time_slice = 3;
 
-pcb_t *current_process;
-
 void setNextTimer(){
-	setTimer(time_slice);
+	setTIMER(time_slice);
 }
 
 void aging(){
-	pcb_t *currentP;
-	list_for_each_entry(currentP, ready_queue, p_next) {
-		currentP->priority = currentP->priority + 1;
+	pcb_t *item;
+	list_for_each_entry(item, ready_queue, p_next) {
+		item->priority = item->priority + 1;
 	}
 }
 
@@ -27,13 +21,13 @@ void schedule(){
 	setNextTimer();
 
 	if(!emptyProcQ(ready_queue)){
-		current_process = (pcb_t*) removeProcQ(ready_queue);
+		pcb_t *current_process = (pcb_t*) removeProcQ(ready_queue);
 
 		current_process->priority = current_process->original_priority;
 
 		aging();
 
-		log_process_order(current_process);
+		log_process_order(current_process->original_priority);
 
 		LDST(&(current_process->p_s));
 	} else {
